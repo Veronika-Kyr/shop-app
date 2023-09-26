8c6a795dee576130f67fb8c608d676628bf257fc
Veronika-Kyr <106926961+Veronika-Kyr@users.noreply.github.com>
2023-09-26 22:32:26 +0300

    Correct server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require("json-server");
+/*const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 
@@ -33,3 +33,20 @@ server.use(router)
 server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
 })*/
+
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3000;
+
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
